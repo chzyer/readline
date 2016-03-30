19657124c7522d51252a8e942d2cb3f8b78ce0ad
chzyer <0@0xdf.com>
2016-03-31 00:19:18 +0800

    remove test for SplitByMultiLine

--- utils_test.go
+++ utils_test.go
@@ -1,15 +1,1 @@
 package readline
-
-import (
-	"reflect"
-	"testing"
-)
-
-func TestSplitByMultiLine(t *testing.T) {
-	rs := []rune("hello!bye!!!!")
-	expected := []string{"hell", "o!", "bye!", "!!", "!"}
-	ret := SplitByMultiLine(0, 6, 4, rs)
-	if !reflect.DeepEqual(ret, expected) {
-		t.Fatal(ret, expected)
-	}
-}
